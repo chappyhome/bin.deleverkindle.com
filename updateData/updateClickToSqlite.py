2ded033194bf87937c75758fd21beddc8ade3ae3
chappyhome <sgqjpw@gmail.com>
2013-09-30 16:46:17 +0800

    增加crontab update click to sqlite

---
+++ updateData/updateClickToSqlite.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*- 
+
+import sqlite3
+import redis
+from os import path
+import ConfigParser
+
+pool = redis.ConnectionPool(host='127.0.0.1', port=6379)  
+r = redis.Redis(connection_pool=pool)
+
+cf = ConfigParser.ConfigParser()
+cf.read("config.conf")
+
+CALIBRE_ALL_BOOKS_SET  = cf.get("key", "CALIBRE_ALL_BOOKS_SET")
+data_save_repository = cf.get("path", "data_save_repository")
+
+
+if path.exists(data_save_repository):
+		try:
+			conn = sqlite3.connect(data_save_repository)
+			conn.row_factory = sqlite3.Row
+			cur = conn.cursor()
+			sql = 'create table if not exists calibre_id_click (id INTEGER PRIMARY KEY, click INTEGER)'
+			cur.execute(sql)
+			conn.commit()
+
+			rows = r.zrange(CALIBRE_ALL_BOOKS_SET, 0, -1, withscores=True)
+			insert_sql = 'INSERT OR REPLACE INTO calibre_id_click VALUES ({0}, {1})'
+			for row in rows:
+				sql = insert_sql.format(row[0], row[1])
+				cur.execute(sql)
+				conn.commit()
+		except sqlite3.Error, e:
+			print "Error %s:" % e.args[0]
